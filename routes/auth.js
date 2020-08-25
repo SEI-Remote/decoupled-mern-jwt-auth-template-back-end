5079a83f3124f5bc2a8d7ea7855ed96cede9e95d
ManliestBen <benjamintmanley@gmail.com>
2020-08-25 09:42:36 -0500

    fix bugs

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,7 @@ router.get(
 router.get(
   "/google/oauth2callback",
   passport.authenticate("google", {
-    successRedirect: "/games",
+    successRedirect: "/users",
     failureRedirect: "/auth/google",
   })
 );
