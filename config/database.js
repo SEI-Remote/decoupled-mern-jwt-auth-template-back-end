079ad2375cbb3f3a42a53a7700662cfcf6aa896e
David <49245298+DavidStinson@users.noreply.github.com>
2020-12-08 09:29:40 -0600

    fix database connection

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 db.on("connected", () => {
