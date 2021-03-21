4568980bcf0440e192cc6597fb05f8a4ece15fe7
DavidStinson <49245298+DavidStinson@users.noreply.github.com>
2021-03-21 16:48:44 -0500

    update readme, remove cookie-parser

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
-const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const session = require("express-session");
 const passport = require("passport");
@@ -31,7 +30,6 @@ app.use(methodOverride("_method"));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 // passport middleware
