7247bc1b844d9a210720810212c94e1e40d7d18d
David <49245298+DavidStinson@users.noreply.github.com>
2021-11-02 17:53:52 -0500

    update title to error page

--- server.js
+++ server.js
@@ -71,7 +71,9 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500)
-  res.render('error')
+  res.render('error', {
+    title: `🎊 ${err.status || 500} Error`
+  })
 })
 
 export { app }
