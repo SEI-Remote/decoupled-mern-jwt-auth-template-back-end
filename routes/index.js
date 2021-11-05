b54cf3962fd3f39731aa4d93eab7577fc9b20991
David <49245298+DavidStinson@users.noreply.github.com>
2021-11-04 19:00:38 -0500

    add whitespace

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 
 const router = Router()
+
 router.get('/', function (req, res) {
   res.render('index', { title: 'Home Page', user: req.user ? req.user : null })
 })
